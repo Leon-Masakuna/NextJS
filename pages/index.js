f21771a4b364d549f04154c8e492a28ad6df05b0
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-10 15:25:22 +0100

    feat : index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Place your order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Hello World</h1>
@@ -10,6 +17,7 @@ function Home() {
       <Link href='/product'>
         <h4>Product</h4>
       </Link>
+      <button onClick={handleClick}>Place order</button>
     </div>
   );
 }
