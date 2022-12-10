2adfcde0603af99f76eecacb69faf37c0522d701
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-10 15:26:27 +0100

    feat : product page index

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,21 @@
-function ProductList() {
+import Link from "next/link";
+
+function ProductList({ productId = 100 }) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href='/'>Home</Link>
+      <Link href='/product/1'>
+        <h2>Product 1</h2>
+      </Link>
+      <Link href='/product/2'>
+        <h2>Product 2</h2>
+      </Link>
+      <Link href='/product/3'>
+        <h2>Product 3</h2>
+      </Link>
+      <Link href={`/product/${productId}`}>
+        <h2>Product cent</h2>
+      </Link>
     </>
   );
 }
